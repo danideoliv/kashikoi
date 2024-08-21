7afc26299742d819dd2c24f76b372dc4d1709af4
Daniel Guerra <80645635+danideoliv@users.noreply.github.com>
2024-08-21 13:57:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function overlay() {
+	if (document.getElementById("overlay").style.display === "block") {
+		document.getElementById("overlay").style.display = "none";
+	} else {
+		document.getElementById("overlay").style.display = "block";
+	}
+}
