95743a45717020d384bce34fb624aa8e17b5ede0
danideoliv <dguerra2600@gmail.com>
2024-09-06 15:20:51 -0300

    Mudança completa do design / v1.5.3

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-function overlay() {
-	if (document.getElementById("overlay").style.display === "block") {
-		document.getElementById("overlay").style.display = "none";
-	} else {
-		document.getElementById("overlay").style.display = "block";
-	}
-}
+var switchBtn = true;
+
+function dropdown() {
+  const dropConStyle = `
+  transform: translateY(0px);
+  opacity: 1;
+  transition: opacity 230ms ease-in-out, transform 150ms ease-in-out, visibility 0ms;
+  visibility: visible;
+  `;
+
+  const selectGrade = document.querySelector(".drop-option");
+  const selectBtn = document.querySelector(".dropbtn");
+  const dropContent = document.querySelector(".dropdown-content");
+
+  rotation = switchBtn ? 180 : 0;
+
+  selectBtn.style.transform = `rotate(${rotation}deg)`;
+
+  if (switchBtn) {
+    selectGrade.style.color = "rgba(255, 255, 255, 0.4)";
+    dropContent.style.cssText = dropConStyle;
+    document.styleSheets[5].insertRule(".drop-option::before { height: calc(100% - 4px); }", 0);
+  } else {
+    selectGrade.style.color = "white";
+    dropContent.style.cssText = "visibility: hidden";
+    document.styleSheets[5].removeRule(".drop-option::before { height: calc(100% - 4px); }", 0);
+  };
+
+  switchBtn = !switchBtn;
+};
