35d679f913b27fab7551892604a03c53dab9171a
danideoliv <dguerra2600@gmail.com>
2024-09-10 16:17:53 -0300

    Atualizando algumas coisas. Adição da segunda parte da função afim (beta) / v1.7

--- primeiro-ano/script1.js
+++ primeiro-ano/script1.js
@@ -1,7 +1,17 @@
-function overlay() {
-	if (document.getElementById("overlay").style.display === "block") {
-		document.getElementById("overlay").style.display = "none";
-	} else {
-		document.getElementById("overlay").style.display = "block";
-	}
-}
+/*import katex from "https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/katex.mjs";
+
+export function funcAfimPts(x1, y1, x2, y2, div) {
+  let a = (y2 - y1) / (x2 - x1);
+  let b = y1 - a * x1;
+  
+  let solution = `
+  P(${x1}, ${y2}) Q(${x2}, ${y2})
+  \\newline
+  \\newline
+  \\frac{${y2}-${y1}}{${x2}-${x1}}
+  `;
+  
+  return katex.render(solution, div, { 
+    displayMode: true 
+  });
+}*/
